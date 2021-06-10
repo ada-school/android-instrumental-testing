ee7eb7c2b2045d17edd7172aa76eb81dd131a326
sancarbar <sancarbar@gmail.com>
2021-06-10 18:54:15 -0500

    5. Implemented instrumental test for authentication (successful and failed)

--- app/src/androidTest/java/org/adaschool/instrumentaltesting/MainActivityBehaviourTest.kt
+++ app/src/androidTest/java/org/adaschool/instrumentaltesting/MainActivityBehaviourTest.kt
@@ -3,11 +3,11 @@ package org.adaschool.instrumentaltesting
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.*
 import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.matcher.ViewMatchers.hasErrorText
-import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.SmallTest
+import org.hamcrest.CoreMatchers.not
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -30,5 +30,26 @@ class MainActivityBehaviourTest {
         closeSoftKeyboard()
         onView(withId(R.id.login_button)).perform(click())
         onView(withId(R.id.email)).check(matches(hasErrorText(TestsUtils.getResourceString(R.string.invalid_input))))
+    }
+
+    @Test
+    fun authenticationFailedThenErrorMessageShown() {
+        onView(withId(R.id.email)).perform(clearText(), typeText("santiago@mail.com"))
+        onView(withId(R.id.password)).perform(clearText(), typeText("password"))
+        closeSoftKeyboard()
+        onView(withId(R.id.login_button)).perform(click())
+        Thread.sleep(6_000)
+        onView(withId(R.id.errorMessage)).check(matches(isDisplayed()))
+    }
+
+
+    @Test
+    fun authenticationSuccessfulThenErrorMessageNotShown() {
+        onView(withId(R.id.email)).perform(clearText(), typeText("test@mail.com"))
+        onView(withId(R.id.password)).perform(clearText(), typeText("password"))
+        closeSoftKeyboard()
+        onView(withId(R.id.login_button)).perform(click())
+        Thread.sleep(6_000)
+        onView(withId(R.id.errorMessage)).check(matches(not(isDisplayed())))
     }
 }
